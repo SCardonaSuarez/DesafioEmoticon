b7f77ff5d2c211f396110ea483aa2c4a394be57e
Santo <santicardonas777@gmail.com>
2021-06-04 17:08:31 -0500

    Se realizo el desafio de Emojis

---
+++ Desafio1/emoticones/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title= '🤪 Emoji 🤪';
+  happy= false;
+  sad  = false;
+  crazy= false;
+}
